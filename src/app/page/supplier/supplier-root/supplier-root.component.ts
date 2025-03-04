2698109a8b29220d7a3f99ec4f3689413981e2f2
icet-sharadha <icet.sharadha@gmail.com>
2025-03-04 10:59:04 +0530

    root

---
+++ src/app/page/supplier/supplier-root/supplier-root.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-supplier-root',
+  imports: [],
+  templateUrl: './supplier-root.component.html',
+  styleUrl: './supplier-root.component.css'
+})
+export class SupplierRootComponent {
+
+}
---
+++ src/app/page/user/user-root/user-root.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-root',
+  imports: [],
+  templateUrl: './user-root.component.html',
+  styleUrl: './user-root.component.css'
+})
+export class UserRootComponent {
+
+}
