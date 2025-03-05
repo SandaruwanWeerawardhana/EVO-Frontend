b10acc4ef5d5bcef8643e8b7da12d96c38ffc023
poorviUvindhya <poorviwejeweera@gmail.com>
2025-03-05 12:06:23 +0530

    Rating and Reviews

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SupplierRootComponent } from './page/supplier/supplier-root/supplier-root.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SupplierRootComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/page/supplier/supplier-root/supplier-root.component.ts
+++ src/app/page/supplier/supplier-root/supplier-root.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RatingReviewsComponent } from '../rating-reviews/rating-reviews.component';
 
 @Component({
   selector: 'app-supplier-root',
-  imports: [],
+  imports: [RatingReviewsComponent],
   templateUrl: './supplier-root.component.html',
   styleUrl: './supplier-root.component.css'
 })
