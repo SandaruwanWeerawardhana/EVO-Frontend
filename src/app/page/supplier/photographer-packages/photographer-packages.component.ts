91f312f316ecfe7858274d4db51db1baa7289eb0
gihani571 <gihanimadhubashini75@gmail.com>
2025-03-05 12:01:57 +0530

    create previous work page

---
+++ src/app/page/supplier/photographer-packages/photographer-packages.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photographer-packages',
+  imports: [],
+  templateUrl: './photographer-packages.component.html',
+  styleUrl: './photographer-packages.component.css'
+})
+export class PhotographerPackagesComponent {
+
+}
