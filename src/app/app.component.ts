8b23bf7cff5c812037f921fa2d2db0398263961c
NimeshaT <nimeshaijse2020@gmail.com>
2025-03-05 14:16:56 +0530

    EventSelection,EventSummary,BeautyEssentials pages are created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EventSummaryComponent } from './page/event/event-summary/event-summary.component';
+import { EventSelectionComponent } from './page/event/event-selection/event-selection.component';
+import { BeautyEssentialsInfoComponent } from './page/event/beauty-essentials-info/beauty-essentials-info.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,EventSummaryComponent,EventSelectionComponent,BeautyEssentialsInfoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
