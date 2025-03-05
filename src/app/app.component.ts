476cff24d6477c857a7a23c7b736a39c02a8ebba
NipunBasnayake <nipunsathsara1999@gmail.com>
2025-03-05 11:53:27 +0530

    Implement User Login and Register Pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserRootComponent } from "./page/user/user-root/user-root.component";
+import { LoginComponent } from "./page/user/login/login.component";
+import { SignupComponent } from "./page/user/signup/signup.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserRootComponent, LoginComponent, SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
