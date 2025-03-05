3b5dc38525d2795d6b2d519b0fd5990e2e0111e3
Praneeth1221 <praneethravishka@gmail.com>
2025-03-05 14:26:03 +0530

    profile view first

--- src/app/page/supplier/supplier-root/supplier-root.component.ts
+++ src/app/page/supplier/supplier-root/supplier-root.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ProfileViewComponent } from "../profile-view/profile-view.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-supplier-root',
-  imports: [],
+  imports: [ProfileViewComponent],
   templateUrl: './supplier-root.component.html',
   styleUrl: './supplier-root.component.css'
 })
