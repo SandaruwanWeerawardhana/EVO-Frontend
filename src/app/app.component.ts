a9e1a3d09b6c6140cdf9b690c58c1c034e2dc549
Lahiru20 <learn.kklahirumatara@gmail.com>
2025-03-05 14:05:07 +0530

    entertainment info page and corousel fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { ServiceSelectionComponent } from './page/event/service-selection/service-selection.component';
 import { PhotographerInfoComponent } from './page/event/photographer-info/photographer-info.component';
 import { SupplierRootComponent } from "./page/supplier/supplier-root/supplier-root.component";
+import { EntertainmentInfoComponent } from './page/event/entertainment-info/entertainment-info.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ServiceSelectionComponent, PhotographerInfoComponent, SupplierRootComponent],
+  imports: [RouterOutlet, ServiceSelectionComponent, PhotographerInfoComponent, SupplierRootComponent, EntertainmentInfoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/page/event/entertainment-info/entertainment-info.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entertainment-info',
+  imports: [],
+  templateUrl: './entertainment-info.component.html',
+  styleUrl: './entertainment-info.component.css'
+})
+export class EntertainmentInfoComponent {
+
+}
+
+
