5911a6d0cc055be925dfdecbddd15c23ef64a7a1
binusenevi <binushiumasha@gmail.com>
2025-03-05 13:55:38 +0530

    view-previous-work & detailed -previous work

---
+++ src/app/page/supplier/detailed-previous-work/detailed-previous-work.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detailed-previous-work',
+  imports: [],
+  templateUrl: './detailed-previous-work.component.html',
+  styleUrl: './detailed-previous-work.component.css'
+})
+export class DetailedPreviousWorkComponent {
+
+}
--- src/app/page/supplier/supplier-root/supplier-root.component.ts
+++ src/app/page/supplier/supplier-root/supplier-root.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ViewPreviousWorkComponent } from '../view-previous-work/view-previous-work.component';
 
 @Component({
   selector: 'app-supplier-root',
-  imports: [],
+  imports: [ViewPreviousWorkComponent],
   templateUrl: './supplier-root.component.html',
   styleUrl: './supplier-root.component.css'
 })
