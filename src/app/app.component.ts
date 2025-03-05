5e793118c28bd1b2d77fb8c436969ca4bfe12f85
Udayanga Wijesiri <udayanga.wijesiri@gmail.com>
2025-03-05 13:59:52 +0530

    implement customer-dashboard specific front-end elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserRootComponent } from "./page/user/user-root/user-root.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserRootComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
