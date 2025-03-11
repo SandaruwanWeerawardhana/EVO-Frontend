073ead789d07684cde1b341eac2f27cc0a3cb8f6
Thusithamadhubhashana <thusithamadhubhashana@gmail.com>
2025-03-11 16:08:39 +0530

    supplier_dash_root

--- src/app/page/supplier/dash/dash-root/dash-root.component.ts
+++ src/app/page/supplier/dash/dash-root/dash-root.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, AfterViewInit } from '@angular/core';
+import Chart from 'chart.js/auto';
 @Component({
   selector: 'app-dash-root',
   imports: [],
@@ -7,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrl: './dash-root.component.css'
 })
 export class DashRootComponent {
+  ngAfterViewInit() {
+    this.loadBarChart();
+    this.loadPieChart();
+  }
 
+  loadBarChart() {
+    const barChart = new Chart("barChart", {
+      type: "bar",
+      data: {
+        labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+        datasets: [{
+          label: "Daily Logins",
+          data: [12, 19, 3, 5, 2, 3, 7],
+          backgroundColor: "rgba(75, 192, 192, 0.6)"
+        }]
+      }
+    });
+  }
+
+  loadPieChart() {
+    const pieChart = new Chart("pieChart", {
+      type: "pie",
+      data: {
+        labels: ["Basic", "Premium", "Exclusive"],
+        datasets: [{
+          label: "Popularity",
+          data: [30, 45, 25],
+          backgroundColor: ["#ff6384", "#36a2eb", "#ffce56"]
+        }]
+      }
+    });
+  }
 }
