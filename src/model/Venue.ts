3683006328256a7199ca39561b042eb79adebe6c
Methminpulsara <methminpulsara10@gmail.com>
2025-03-12 12:55:22 +0530

    create venue-pop-up

---
+++ src/model/Venue.ts
@@ -0,0 +1,22 @@
+export default class Venur{
+
+  id:number;
+  supplierId: number;
+  location: string ;
+  type:string;
+
+    // aluthen add kla
+  name:string
+  about:string;
+  capacity:number;  
+
+  constructor(id:number , supplierId:number ,name:string, location :string , type: string , about:string , capacity:number){
+      this.id=id;
+      this.supplierId=supplierId;
+      this.name = name ;
+      this.location=location;
+      this.type= type;
+      this.about = about;
+      this.capacity =capacity;
+  }
+}
