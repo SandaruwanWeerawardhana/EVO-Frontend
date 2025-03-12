6962a26c39f7e5ea63d1348398fdd15c3793ea82
dimantha2004 <Dimanthatheekshana12345@gmail.com>
2025-03-12 11:26:04 +0530

    venue model

--- src/app/page/event/venue-dashboard/venue-dashboard.component.ts
+++ src/app/page/event/venue-dashboard/venue-dashboard.component.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-
+import Venue from '../../../../model/Venue';
 @Component({
-  selector: 'app-venue-dashboard',
-  imports: [RouterLink],
-  templateUrl: './venue-dashboard.component.html',
-  styleUrl: './venue-dashboard.component.css'
+    selector: 'app-venue-dashboard',
+    standalone: true,
+    imports: [RouterLink, CommonModule],
+    templateUrl: './venue-dashboard.component.html',
+    styleUrl: './venue-dashboard.component.css',
 })
 export class VenueDashboardComponent {
 
+  public venue: Venue[] = [
+    {
+        name: "shangrila",
+        id: 1,
+        supplierId: 1,
+        location: "panadura",
+        type: 'VILLA',
+        price:25000,
+        image: "https://cdn.pixabay.com/photo/2025/03/07/13/12/flower-9453063_1280.jpg" 
+    },
+    {
+        name: "galadari",
+        id: 2,
+        supplierId: 1,
+        location: "colombo",
+        type: 'RESTAURANT',
+        price:25000,
+        image: "https://cdn.pixabay.com/photo/2025/03/07/13/12/flower-9453063_1280.jpg" 
+    },
+    {
+        name: "shangrila",
+        id: 3,
+        supplierId: 1,
+        location: "kandy",
+        type: 'HOTEL',
+        price:25000,
+        image: "https://cdn.pixabay.com/photo/2025/03/07/13/12/flower-9453063_1280.jpg" 
+    },
+    {
+        name: "Galadari",
+        id: 4,
+        supplierId: 1,
+        location: "horana",
+        type: 'VILLA',
+        price:25000,
+        image: "https://cdn.pixabay.com/photo/2025/03/07/13/12/flower-9453063_1280.jpg" 
+    }
+];
 }
+
---
+++ src/model/Venue.ts
@@ -0,0 +1,21 @@
+type VenueType = "RESTAURANT" | "VILLA" | "HOTEL";
+
+export default class Venue {
+    name: string;
+    id: number;
+    supplierId: number;
+    location: string;
+    type: VenueType;
+    price:number;
+    image: string; 
+
+    constructor(name: string, id: number, supplierId: number, location: string, type: VenueType,price:number, image: string) {
+        this.name = name;
+        this.id = id;
+        this.supplierId = supplierId;
+        this.location = location;
+        this.type = type;
+        this.price=price;
+        this.image = image; 
+    }
+}
