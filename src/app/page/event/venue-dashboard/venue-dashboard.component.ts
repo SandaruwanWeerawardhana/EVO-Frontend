ee139e397845a0436ffb55e9f35f3e23532d28e2
icet-sharadha <icet.sharadha@gmail.com>
2025-03-14 14:31:46 +0530

    update dev

--- src/app/page/event/venue-dashboard/venue-dashboard.component.ts
+++ src/app/page/event/venue-dashboard/venue-dashboard.component.ts
@@ -11,43 +11,58 @@ import Venue from '../../../../model/Venue';
 })
 export class VenueDashboardComponent {
 
+
+  // this.id=id;
+  // this.supplierId=supplierId;
+  // this.name = name ;
+  // this.location=location;
+  // this.type= type;
+  // this.price=price;
+  // this.image=image;
+  // this.about = about;
+  // this.capacity =capacity;
+
+
+
   public venue: Venue[] = [
     {
+      id: 1,
+      supplierId: 1,
         name: "shangrila",
-        id: 1,
-        supplierId: 1,
         location: "panadura",
         type: 'VILLA',
         price:25000,
-        image: "https://cdn.pixabay.com/photo/2025/03/07/13/12/flower-9453063_1280.jpg" 
+        image: "https://cdn.pixabay.com/photo/2025/03/07/13/12/flower-9453063_1280.jpg",
+        about:"bla bla",
+        capacity:5
     },
-    {
-        name: "galadari",
-        id: 2,
-        supplierId: 1,
-        location: "colombo",
-        type: 'RESTAURANT',
-        price:25000,
-        image: "https://cdn.pixabay.com/photo/2025/03/07/13/12/flower-9453063_1280.jpg" 
-    },
-    {
-        name: "shangrila",
-        id: 3,
-        supplierId: 1,
-        location: "kandy",
-        type: 'HOTEL',
-        price:25000,
-        image: "https://cdn.pixabay.com/photo/2025/03/07/13/12/flower-9453063_1280.jpg" 
-    },
-    {
-        name: "Galadari",
-        id: 4,
-        supplierId: 1,
-        location: "horana",
-        type: 'VILLA',
-        price:25000,
-        image: "https://cdn.pixabay.com/photo/2025/03/07/13/12/flower-9453063_1280.jpg" 
-    }
+    // {
+    //     name: "galadari",
+    //     id: 2,
+    //     supplierId: 1,
+    //     location: "colombo",
+    //     type: 'RESTAURANT',
+    //     price:25000,
+    //     image: "https://cdn.pixabay.com/photo/2025/03/07/13/12/flower-9453063_1280.jpg"
+    // },
+    // {
+    //     name: "shangrila",
+    //     id: 3,
+    //     supplierId: 1,
+    //     location: "kandy",
+    //     type: 'HOTEL',
+    //     price:25000,
+    //     image: "https://cdn.pixabay.com/photo/2025/03/07/13/12/flower-9453063_1280.jpg"
+    // },
+    // {
+    //     name: "Galadari",
+    //     id: 4,
+    //     supplierId: 1,
+    //     location: "horana",
+    //     type: 'VILLA',
+    //     price:25000,
+    //     image: "https://cdn.pixabay.com/photo/2025/03/07/13/12/flower-9453063_1280.jpg"
+    // }
 ];
 }
 
