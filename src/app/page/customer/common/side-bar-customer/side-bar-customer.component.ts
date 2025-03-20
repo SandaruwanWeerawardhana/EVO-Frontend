2b5f941ce34bdddfb31eac2c624bbcfed4a4dcb1
RuwanDilhara <roshansameera916@gmail.com>
2025-03-20 16:11:14 +0530

    create active mode in button

--- src/app/page/customer/common/side-bar-customer/side-bar-customer.component.ts
+++ src/app/page/customer/common/side-bar-customer/side-bar-customer.component.ts
@@ -1,12 +1,23 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar-customer',
-  imports: [RouterLink],
+  imports: [RouterLink,NgClass],
   templateUrl: './side-bar-customer.component.html',
   styleUrl: './side-bar-customer.component.css'
 })
 export class SideBarCustomerComponent {
+  isSelect: number = 0;
+  isSidebarClosed: boolean = false;
 
-}
+  isActive(num: number) {
+    this.isSelect = num;
+  }
+    toggleSidebar() {
+      this.isSidebarClosed = !this.isSidebarClosed;
+    }
+  }
+  
+
