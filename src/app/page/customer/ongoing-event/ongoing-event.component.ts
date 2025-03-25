59305d5ce2f41eeab1addf599c9550542bd4f4dd
SENURA INDEEWARA <senuraindeewara@gmail.com>
2025-03-25 17:00:28 +0530

    update

--- src/app/page/customer/ongoing-event/ongoing-event.component.ts
+++ src/app/page/customer/ongoing-event/ongoing-event.component.ts
@@ -7,11 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './ongoing-event.component.css'
 })
 export class OngoingEventComponent {
-  agenda = [
-    { name: 'Pre-ceremony', startDate: '19/04/2024 09:00', duration: 3 },
-    { name: 'Hair & Makeup', startDate: '19/04/2024 09:00', duration: 1 },
-    { name: 'Vendors Arrive', startDate: '19/04/2024 10:00', duration: 1 },
-    { name: 'Ceremony', startDate: '19/04/2024 12:00', duration: 2 },
-    { name: 'Reception', startDate: '19/04/2024 15:00', duration: 4 }
-  ];
+ 
 }
