938febaea7d25a28d3c730cc2240de319426e487
kaveeshaSanjana <kapilakarunarathna@gmail.com>
2025-03-27 09:13:02 +0530

    Update event summery

--- src/app/page/event/event-summary/event-summary.component.ts
+++ src/app/page/event/event-summary/event-summary.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import EventSummery from '../../../model/EventSummery';
 
 @Component({
   selector: 'app-event-summary',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink , NgFor ],
   templateUrl: './event-summary.component.html',
   styleUrls: ['./event-summary.component.css']
 })
-export class EventSummaryComponent {
-  
+export class EventSummaryComponent implements OnInit {
+  public eventSummery:any = null;
+  event:any = {
+    eventType : "wedding",
+    startDate :"2023/02/02",
+    endDate :"2026/03/07",
+    eventStatus:"Pending"
+  }; 
+  supplers:any[] = [{ 
+    name:"Rocky"
+  }];
+
+
+
+  ngOnInit(): void {
+      this.refeshData();
+  }
+
+  refeshData(){
+
+  }
 }
