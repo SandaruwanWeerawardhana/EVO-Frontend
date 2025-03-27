eaf3d80bd5d03c1e07f8dd7af126a5c7f56392eb
Sash980402 <kesharasasanka120@gmail.com>
2025-03-27 13:46:43 +0530

    wedding

---
+++ src/app/page/event/wedding-form/wedding-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wedding-form',
+  imports: [],
+  templateUrl: './wedding-form.component.html',
+  styleUrl: './wedding-form.component.css'
+})
+export class WeddingFormComponent {
+
+}
