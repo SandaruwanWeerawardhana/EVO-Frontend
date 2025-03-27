a6a851f68b55f1a896a99b04cdfcd8825d2c7106
DESHIKATHIKSHANI <deshikathikshani456@gmail.com>
2025-03-27 14:10:47 +0530

    Created Annivesary Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { VenuePagePopupComponent } from "./page/event/venue-page-popup/venue-page-popup.component";
 
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
