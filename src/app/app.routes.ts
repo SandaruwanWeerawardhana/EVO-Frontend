a34994e1ba669012da47a24dba2e0ec7472d71e7
Sash980402 <kesharasasanka120@gmail.com>
2025-04-01 11:18:26 +0530

    agendaservice

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -48,16 +48,16 @@ import { AuditHistoryComponent } from './page/customer/admin-dashbaord/pages/aud
 import { OverviewComponent } from './page/customer/admin-dashbaord/pages/overview/overview.component';
 import { RequestsComponent } from './page/customer/admin-dashbaord/pages/requests/requests.component';
 import { SupplierPendingRequestComponent } from './page/supplier/dash/supplier-pending-request/supplier-pending-request.component';
-<<<<<<< HEAD
+
 import { ViewInventoryComponent } from './page/supplier/view-inventory/view-inventory.component';
 import { WeddingFormComponent } from './page/event/wedding-form/wedding-form.component';
-=======
+
 
 import { MessageComponent } from './page/customer/admin-dashbaord/pages/message/message.component';
 
 // import { ViewInventoryComponent } from './page/supplier/view-inventory/view-inventory.component';
 
->>>>>>> a223f0fa149da7f74f80cee1520a97e762cb5e2d
+
 
 export const routes: Routes = [
   {
--- src/service/agendaService.ts
+++ src/service/agendaService.ts
@@ -2,6 +2,8 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import User from "../app/model/User";
+import { Session } from "@supabase/supabase-js";
+import { AgendaComponent } from "../app/page/event/agenda/agenda.component";
 
 @Injectable({
     providedIn: 'root'
@@ -10,9 +12,23 @@ export class AgendaService {
 
     constructor(private http:HttpClient) {
     }
-    loaduser():Observable<User[]>{
-        return this.http.get<User[]>('${env.baseUrl}//userController/userId/{id}');
+    loadevent():Observable<Event[]>{
+        return this.http.get<Event[]>('${env.baseUrl}/api/event/search/{id}');
     }
+    savesession(session:Session):Observable<Session>{
+        return this.http.post<Session>('${env.baseUrl}/api/event/agenda/{agendaId}/add-task', {session});
+    }
+    loadsession(agendaId: number, taskId: number):Observable<Session[]>{
+        return this.http.get<Session[]>('${env.baseUrl}/api/event/agenda/{agendaId}/task/{taskId}');
+    }
+    deletesession():Observable<Session>{
+        return this.http.delete<Session>('${env.baseUrl}/api/event/agenda/{agendaId}/task/delete/{taskId}');
+    }
+    updateagenda(agenda:AgendaComponent):Observable<AgendaComponent>{
+        return this.http.put<AgendaComponent>('${env.baseUrl}/api/event/agenda/update', {agenda});
+    }
+    
+
     
 }
 
