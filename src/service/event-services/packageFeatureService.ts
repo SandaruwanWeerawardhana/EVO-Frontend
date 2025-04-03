a7726d5852f9200033a5fcdb1f8545c0a3f325b3
Kaveesha Sanjana <kavishasanjana22@gmail.com>
2025-04-03 12:58:48 +0530

    Remove repeating of base url

--- src/service/event-services/packageFeatureService.ts
+++ src/service/event-services/packageFeatureService.ts
@@ -11,21 +11,21 @@ export class PackageFeatureService {
     constructor(private http: HttpClient) {
     }
 
-    baseurl = `${environment.baseUrl}`;
+    baseurl = `${environment.baseUrl}/api/supplier`;
 
     getAllPackageFeatures(): Observable<PackageFeature[]> {
-        return this.http.get<PackageFeature[]>(`${this.baseurl}/api/supplier/package-feature`);
+        return this.http.get<PackageFeature[]>(`${this.baseurl}/package-feature`);
     }
     savePackageFeature(packageFeature: PackageFeature, packageID: number): Observable<PackageFeature> {
-        return this.http.post<PackageFeature>(`${this.baseurl}/api/supplier/${packageID}/package-feature`, packageFeature);
+        return this.http.post<PackageFeature>(`${this.baseurl}/${packageID}/package-feature`, packageFeature);
     }
     deletePackageFeature(id: number): Observable<PackageFeature> {
-        return this.http.delete<PackageFeature>(`${this.baseurl}/api/supplier/package-feature/${id}`);
+        return this.http.delete<PackageFeature>(`${this.baseurl}/package-feature/${id}`);
     }
     updatePackageFeature(packageFeature: PackageFeature, packageID: number): Observable<PackageFeature> {
-        return this.http.put<PackageFeature>(`${this.baseurl}/api/supplier/${packageID}/package-feature`, packageFeature);
+        return this.http.put<PackageFeature>(`${this.baseurl}/${packageID}/package-feature`, packageFeature);
     }
     searchPackageFeatureById(id: number): Observable<PackageFeature[]> {
-        return this.http.get<PackageFeature[]>(`${this.baseurl}/api/supplier/package-feature/${id}`);
+        return this.http.get<PackageFeature[]>(`${this.baseurl}/package-feature/${id}`);
     }
 }
