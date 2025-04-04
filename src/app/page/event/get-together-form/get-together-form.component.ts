1f7a1c21688208c7ec965e3879aaef8f753629f5
hasa10 <rasitha8000@gmail.com>
2025-04-04 10:52:12 +0530

    Get_Together_Details

--- src/app/page/event/get-together-form/get-together-form.component.ts
+++ src/app/page/event/get-together-form/get-together-form.component.ts
@@ -1,12 +1,51 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { from } from 'rxjs';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-get-together-form',
-  imports: [RouterLink],
+  imports: [RouterLink , FormsModule,CommonModule],
   templateUrl: './get-together-form.component.html',
   styleUrl: './get-together-form.component.css'
 })
 export class GetTogetherFormComponent {
 
+  formData = {
+    title: '',
+    description: '',
+    eventDate: '',
+    startTime: '',
+    endTime: '',
+    capacity: 0
+  };
+
+  submit() {
+    if (
+      !this.formData.title ||
+      !this.formData.description ||
+      !this.formData.eventDate ||
+      !this.formData.startTime ||
+      !this.formData.endTime ||
+      this.formData.capacity < 1
+    ) {
+      console.log('Form is invalid');
+      return;
+    }
+    Swal.fire({
+      title: 'Success!',
+      text: 'Event submitted successfully!',
+      icon: 'success',
+      confirmButtonText: 'OK',
+      customClass: {
+        confirmButton: 'btn btn-primary'
+      },
+      timer: 3000 
+    });
+    console.log('Form is valid');
+    console.log(this.formData);
+  }
 }
