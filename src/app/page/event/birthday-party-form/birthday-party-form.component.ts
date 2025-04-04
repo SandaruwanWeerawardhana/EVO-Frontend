aab064ff82b24bc7ca3d737860d07f9da973f5c6
Sash980402 <kesharasasanka120@gmail.com>
2025-04-04 14:41:49 +0530

    new birthday

--- src/app/page/event/birthday-party-form/birthday-party-form.component.ts
+++ src/app/page/event/birthday-party-form/birthday-party-form.component.ts
@@ -1,57 +1,80 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-birthday-party-form',
-  imports: [RouterLink,FormsModule],
+  imports: [RouterLink, FormsModule],
   templateUrl: './birthday-party-form.component.html',
   styleUrl: './birthday-party-form.component.css'
 })
-
 
 export class BirthdayPartyFormComponent {
   ownerName: string = '';
   eventdate: Date = new Date();
   headcount: number = 0;
-  starttime:Date = new Date();
-  endtime:Date = new Date();
+  starttime: Date = new Date();
+  endtime: Date = new Date();
 
-  formdata:FormData= new FormData();
- 
+  formdata: FormData = new FormData();
+
   submitbuttonOnClick() {
     if (!this.ownerName) {
-      alert('Owner Name is required!');
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Owner Name is required!',
+      });
       return;
     }
     if (!this.eventdate) {
-      alert('Event Date is required!');
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Event Date is required!',
+      });
       return;
     }
     if (this.headcount <= 0) {
-      alert('Headcount should be greater than 0!');
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Headcount should be greater than 0!',
+      });
       return;
     }
     if (!this.starttime || !this.endtime) {
-      alert('Start Time and End Time are required!');
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Start Time and End Time are required!',
+      });
       return;
     }
     if (this.starttime >= this.endtime) {
-      alert('Start Time must be earlier than End Time!');
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Start Time must be earlier than End Time!',
+      });
       return;
     }
 
-    // Append data
+  
     this.formdata.append('ownerName', this.ownerName);
     this.formdata.append('eventdate', this.eventdate.toString());
     this.formdata.append('headcount', this.headcount.toString());
     this.formdata.append('starttime', this.starttime.toString());
     this.formdata.append('endtime', this.endtime.toString());
 
-    alert('Form submitted successfully!');
+    Swal.fire({
+      icon: 'success',
+      title: 'Form submitted successfully!',
+      showConfirmButton: false,
+      timer: 1500,
+    });
+
     console.log('Form Data:', this.formdata);
-
-   
-  }
-
+    
 }
