9acad5477a781ff83645d4f39ee3128ea4351ff6
Kaveesha Sanjana <kavishasanjana22@gmail.com>
2025-04-09 02:35:18 +0530

    Update timeline.component.ts

--- src/app/common/timeline/timeline.component.ts
+++ src/app/common/timeline/timeline.component.ts
@@ -24,16 +24,6 @@ export class TimelineComponent {
        title: 'Event Title 2',
        description: 'Description for Event 2'
      }
-    // {
-    //   timestamp: new Date('2025-01-01'),
-    //   title: 'Event Title 1',
-    //   description: 'Description for Event 1'
-    // },
-    // {
-    //   timestamp: new Date('2025-02-01'),
-    //   title: 'Event Title 2',
-    //   description: 'Description for Event 2'
-    // }
   ];
 
   scrollRight() {
